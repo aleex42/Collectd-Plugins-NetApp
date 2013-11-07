b51f3483c740e8e3215865cca64f7c3c9afcaad3
root <root@raff.ich-halt.net>
2013-11-07 14:51:42 +0100

    Added cDOT NIC

--- Collectd/Plugins/NetApp/NIC.pm
+++ Collectd/Plugins/NetApp/NIC.pm
@@ -151,7 +151,7 @@ sub nic_module {
                     my @nic_value = @{ $nic_value_ref };
 
                     plugin_dispatch_values({
-                            plugin => 'interface_new',
+                            plugin => 'interface',
                             plugin_instance => $nic,
                             type => 'if_octets',
                             values => [$nic_value[0], $nic_value[1]],

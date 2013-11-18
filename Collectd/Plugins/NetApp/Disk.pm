b3497e37c67612d86404025f956907fd4f351fb7
root <root@raff.ich-halt.net>
2013-11-18 17:51:32 +0100

    Fixed Disk Busy

--- Collectd/Plugins/NetApp/Disk.pm
+++ Collectd/Plugins/NetApp/Disk.pm
@@ -25,8 +25,66 @@ use lib "/usr/lib/netapp-manageability-sdk-5.1/lib/perl/NetApp";
 use NaServer;
 use NaElement;
 
-use Data::Dumper;
 use Config::Simple;
+
+sub smode_disk {
+
+    my $hostname = shift;
+
+    my $in = NaElement->new("perf-object-get-instances");
+    $in->child_add_string("objectname","disk");
+    my $counters = NaElement->new("counters");
+    $counters->child_add_string("counter","raid_name");
+    $counters->child_add_string("counter","disk_busy");
+    $counters->child_add_string("counter","base_for_disk_busy");
+    $in->child_add($counters);
+
+    my $out = connect_filer($hostname)->invoke_elem($in);
+
+    my ($raid_name, $disk_busy, $base_disk_busy);
+
+    my $instances_list = $out->child_get("instances");
+
+    if($instances_list){
+
+        my @instances = $instances_list->children_get();
+
+        my %max_percent;
+
+        foreach my $disk (@instances){
+
+            my $counters_list = $disk->child_get("counters");
+            if($counters){
+
+                my @counters =  $counters_list->children_get();
+    
+                my %values = (raid_name => undef, disk_busy => undef, base_for_disk_busy => undef);
+    
+                foreach my $counter (@counters){
+                    my $key = $counter->child_get_string("name");
+                    if(exists $values{$key}){
+                        $values{$key} = $counter->child_get_string("value");
+                    }
+                }
+    
+                my (undef, $aggr_key, undef, undef, $disc) = split m!/!, $values{raid_name};
+                next unless $aggr_key && $disc;
+    
+                if ($max_percent{$aggr_key}){
+                    if ($values{disk_busy} > $max_percent{$aggr_key}){
+                        $max_percent{$aggr_key} = $values{disk_busy};
+                    }
+                } else {
+                    $max_percent{$aggr_key} = [$values{disk_busy}, $values{base_for_disk_busy}];
+                }
+            }
+        }
+        return \%max_percent;
+    } else {
+        return undef;
+    }
+}
+
 
 sub cdot_disk {
     
@@ -212,15 +270,14 @@ sub disk_module {
 
                 foreach my $aggr (keys %$disk_result){
 
-#                    my $aggr_value_ref = $disk_result->{$aggr};
-#                    my @aggr_value = @{ $aggr_value_ref };
-                    my $aggr_value = $disk_result->{$aggr};
+                    my $aggr_value_ref = $disk_result->{$aggr};
+                    my @aggr_value = @{ $aggr_value_ref };
 
                     plugin_dispatch_values({
                             plugin => 'disk_busy',
                             type => 'percent',
-#                            type_instance => $aggr,
-                            values => [$aggr_value],
+                            type_instance => $aggr,
+                            values => [@aggr_value],
                             interval => '30',
                             host => $hostname,
                             });
